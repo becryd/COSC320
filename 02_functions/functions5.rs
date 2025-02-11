dd22c6765608c0fa5463c7e99aee40ac417633e3
Meera's Orbit <69226897+becryd@users.noreply.github.com>
2025-02-11 09:32:04 +0400

    Add files via upload

---
+++ 02_functions/functions5.rs
@@ -0,0 +1,9 @@
+// TODO: Fix the function body without changing the signature.
+fn square(num: i32) -> i32 {
+    num * num //the syntax of a return value is without a semicolon
+}
+
+fn main() {
+    let answer = square(3);
+    println!("The square of 3 is {answer}");
+}
